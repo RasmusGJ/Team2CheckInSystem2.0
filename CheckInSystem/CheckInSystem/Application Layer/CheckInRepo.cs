c08ceb027d1b030fb7aefb1a63743619625262e3
Andreas Bosteen <andr63c1@edu.ucl.dk>
2020-02-20 13:57:02 +0100

    Update CheckInRepo.cs

--- CheckInSystem/CheckInSystem/Application Layer/CheckInRepo.cs
+++ CheckInSystem/CheckInSystem/Application Layer/CheckInRepo.cs
@@ -6,17 +6,18 @@ namespace CheckInSystem.Application_Layer
 {
     public class CheckInRepo
     {
-        private bool CheckIfCheckedIn()
+        public List<CheckIn> CheckIns = new List<CheckIn>();
+        private bool CheckIfCheckedIn(Person person)
         {
             return true;
         }
 
-        public void CheckIn() 
+        public void CheckIn(CheckIn checkIn) 
         { 
 
         }
 
-        public void CheckOut()
+        public void CheckOut(Person person)
         {
 
         }
