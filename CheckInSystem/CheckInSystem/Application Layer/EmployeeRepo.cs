9b40026e0effc3af46419a9c775180dda9650c51
RasmusGJ <60926479+RasmusGJ@users.noreply.github.com>
2020-02-27 13:30:53 +0100

    Filter på ListView

--- CheckInSystem/CheckInSystem/Application Layer/EmployeeRepo.cs
+++ CheckInSystem/CheckInSystem/Application Layer/EmployeeRepo.cs
@@ -18,9 +18,10 @@ namespace CheckInSystem.Application_Layer
             using (SqlConnection conn = new SqlConnection(ConnectionString))
             {
                 //Selects all from Employee tabel in database
-                string pinCodeQuery = "SELECT Employee.Id, Employee.Name, Employee.Email, Employee.MobilePhone, Employee.LandlinePhone, Employee.Initials, Employee.PinCode, Role.Title AS RoleTitle " +
+                string pinCodeQuery = "SELECT Employee.Id, Employee.Name, Employee.Email, Employee.MobilePhone, Employee.LandlinePhone, Employee.Initials, Employee.PinCode, Role.Title AS RoleTitle, Department.Title AS DepartmentTitle " +
                     "FROM Employee " +
-                    "INNER JOIN Role ON Employee.Role_Id = Role.Id";
+                    "INNER JOIN Role ON Employee.Role_Id = Role.Id " +
+                    "INNER JOIN Department ON Role.Department_Id = Department.Id";
 
                 SqlCommand command = new SqlCommand(pinCodeQuery, conn);
                 conn.Open();
@@ -31,13 +32,14 @@ namespace CheckInSystem.Application_Layer
                         Employee employee = new Employee();
 
                         //Adds relavent column to properties in Employee object. Then adds an employee to employees list.
-                        employee.Name = reader.GetString("Name");
-                        employee.Initials = reader.GetString("Initials");
-                        employee.LandlinePhone = reader.GetString("LandlinePhone");
-                        employee.PinCode = reader.GetString("PinCode");
-                        employee.Email = reader.GetString("Email");
-                        employee.Role = reader.GetString("RoleTitle");
-                        employee.MobilePhone = reader.GetString("MobilePhone");
+                        employee.Name = String.IsNullOrEmpty(reader.GetString("Name")) ? " " : reader.GetString("Name");
+                        employee.Initials = String.IsNullOrEmpty(reader.GetString("Initials")) ? " " : reader.GetString("Initials");
+                        employee.LandlinePhone = String.IsNullOrEmpty(reader.GetString("LandlinePhone")) ? " " : reader.GetString("LandlinePhone");
+                        employee.PinCode = String.IsNullOrEmpty(reader.GetString("PinCode")) ? " " : reader.GetString("PinCode");
+                        employee.Email = String.IsNullOrEmpty(reader.GetString("Email")) ? " " : reader.GetString("Email");
+                        employee.Role = String.IsNullOrEmpty(reader.GetString("RoleTitle")) ? " " : reader.GetString("RoleTitle");
+                        employee.MobilePhone = String.IsNullOrEmpty(reader.GetString("MobilePhone")) ? " " : reader.GetString("MobilePhone");
+                        employee.Department = String.IsNullOrEmpty(reader.GetString("DepartmentTitle")) ? " " : reader.GetString("DepartmentTitle");
                         employee.Id = reader.GetInt32("Id");
                         employees.Add(employee);
                     }
--- CheckInSystem/CheckInSystem/GuestRequestWindow.xaml.cs
+++ CheckInSystem/CheckInSystem/GuestRequestWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using CheckInSystem.Application_Layer;
+using CheckInSystem.Domain_Layer;
 
 namespace CheckInSystem
 {
@@ -23,7 +24,27 @@ namespace CheckInSystem
             InitializeComponent();
             WindowState = WindowState.Maximized;
             EmployeeRepo employeeRepo = new EmployeeRepo();
-            DataContext = employeeRepo;
+            DataContext = employeeRepo;            
+            listView.ItemsSource = employeeRepo.employees;
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(listView.ItemsSource);
+            view.Filter = UserFilter;
+        }
+
+        private bool UserFilter(object item)
+        {
+            if (String.IsNullOrEmpty(nameFilter.Text))
+                return true;
+            if (String.IsNullOrEmpty(departmentFilter.Text))
+                return true;
+            else if (!String.IsNullOrEmpty(departmentFilter.Text))
+                return ((item as Employee).Department.IndexOf(departmentFilter.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+            else
+                return ((item as Employee).Name.IndexOf(nameFilter.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        private void txtFilter_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(listView.ItemsSource).Refresh();
         }
     }
 }
