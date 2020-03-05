22201f9dd059a4401c49f7f5b1788fabff221dd3
Andreas Bosteen <andr63c1@edu.ucl.dk>
2020-03-05 14:13:10 +0100

    AppointmentRepo

---
+++ CheckInSystem/CheckInSystem/Domain Layer/Room.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckInSystem.Domain_Layer
+{
+    class Room
+    {
+        string Name;
+        string Email;
+    }
+}
--- CheckInSystem/CheckInSystem/PinCodeWindow.xaml.cs
+++ CheckInSystem/CheckInSystem/PinCodeWindow.xaml.cs
@@ -41,12 +41,6 @@ namespace CheckInSystem
             UpdateCircles();
 
         }        
-
-        public void GetController(Controller newcontroller)
-        {
-            controller = newcontroller;
-        }
-
         public void UpdateCircles()
         {
             switch (TextBoxNumPad.Text.Length)
