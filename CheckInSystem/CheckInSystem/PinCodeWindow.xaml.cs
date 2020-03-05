dfea27aecc87ff864a925e5963c4e94c90e84f74
Jacob Sørensen <jaco221r@edu.ucl.dk>
2020-03-05 12:35:22 +0100

    Revert "Update PinCodeWindow.xaml.cs"
    This reverts commit df1e189822c026a7778a9d709520fe9d407e7cb6.

--- CheckInSystem/CheckInSystem/PinCodeWindow.xaml.cs
+++ CheckInSystem/CheckInSystem/PinCodeWindow.xaml.cs
@@ -26,6 +26,10 @@ namespace CheckInSystem
             InitializeComponent();
             WindowState = WindowState.Maximized;
         }
+        public void GetController(Controller newController)
+        {
+            controller = newController;
+        }
 
         private void AddNum(object sender, RoutedEventArgs e)
         {
